9ef8d4659a28f59de72892f8592c14290883b66b
yanzhenl <li.yanzhe@northeastern.edu>
2023-04-01 15:28:00 -0700

    a9 Mongo sp23

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
- || 'mongodb://127.0.0.1:27017/tuiter'
+import dotenv from "dotenv";
+dotenv.config();
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING 
+  || "mongodb://127.0.0.1:27017/tuiter";
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
